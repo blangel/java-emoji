1a2af2e0cd7d8c9fd28bd3a5919c7b40a35134c7
blangel <langelb@gmail.com>
2015-11-15 14:21:04 -0500

    * Adding characteristics to emoji.

---
+++ src/main/java/net/ocheyedan/Characteristic.java
@@ -0,0 +1,21 @@
+package net.ocheyedan;
+
+/**
+ * User: blangel
+ * Date: 11/15/15
+ * Time: 1:46 PM
+ */
+public enum Characteristic {
+
+    Animal,
+
+    Person,
+
+    Face,
+
+    Food,
+
+    Plant,
+
+    Machine
+}
